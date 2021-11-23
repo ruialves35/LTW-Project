2f0b51107cf2b83e4e16ec38f457a6b5b9356a4c
up201905853 <up201905853@fe.up.pt>
2021-11-23 09:46:48 +0000

    Fix bug on the board and changing number seeds

--- scripts/onload.js
+++ scripts/onload.js
@@ -1,22 +1,31 @@
-var DEFAULT_SEEDS_NUM = 6;
+var DEFAULT_SEEDS_NUM = 1;
 var DEFAULT_CAVS_NUM = 6;
+
+const $ = (selector) => document.querySelector(selector)
+
+function getNumSeeds () {
+    DEFAULT_SEEDS_NUM = document.getElementById("seeds_number").value;
+    load();
+}
 
 function load () {
     document.getElementById("body").classList.remove("preload");
-    var board_children = document.getElementById("board").getElementsByClassName("cell-container");
+    const board_children = document.getElementById("board").getElementsByClassName("cell-container");
     
     for (var i = 0; i < board_children.length; i++) {
-        var cells = board_children[i].children;
+        const cells = board_children[i].children;
 
         for (var j = 0; j < cells.length; j++) {
-            var cell = cells[j];
+            const cell = cells[j];
             for (var k = 0; k < DEFAULT_SEEDS_NUM; k++) {
-                var div = document.createElement('div');
+                const div = document.createElement('div');
                 
-                var angle = Math.floor(Math.random() * 360);
-                var offset = Math.floor(Math.random() * 100 - 50);
+                const angle = Math.floor(Math.random() * 360)
+                const offsetX = Math.floor(Math.random() * 100 - 50)
+                const offsetY = Math.floor(Math.random() * 200 - 50)
+                const offsetMultiplier = Math.floor(Math.random()*2 - 1);
                 div.classList.add("seed");
-                div.style.transform = "translateX(" + offset + "%) rotate(" + angle + "deg)";
+                div.style.transform = "translateX(" + offsetX + "%) translateY(" + offsetMultiplier*offsetY +"%) rotate(" + angle + "deg)";
                 cell.appendChild(div);
             }
         }
