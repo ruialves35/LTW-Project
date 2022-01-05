0b8c38b06322351b8356e98a5b6c31f6d2d4f975
David Preda <dtpreda30@gmail.com>
2022-01-05 19:55:46 +0000

    Add dynamic server messages

--- scripts/side_menu.js
+++ scripts/side_menu.js
@@ -1,3 +1,29 @@
+function sendMessage(text) {
+    var msg = document.createElement('div');
+    msg.id = "message";
+    msg.classList.add("message");
+
+    var header = document.createElement('span');
+    header.classList.add("form-header");
+
+    var title = document.createElement('h2');
+    title.innerHTML = "Server Message";
+    
+    var icon = document.createElement('i');
+    icon.classList.add("bi", "bi-x-circle");
+    icon.onclick = function () { hide("message"); setTimeout(function () { document.getElementById("message").parentElement.removeChild(msg); }, 400); };
+
+    header.appendChild(title);
+    header.appendChild(icon);
+
+    msg.appendChild(header);
+    msg.appendChild(document.createTextNode(text));
+
+    document.getElementById("body").appendChild(msg);
+
+    setTimeout(function () { show("message"); });
+}
+
 function show(id) {
     var bd = document.getElementById("container");
     var nav = document.getElementById("navbar");
