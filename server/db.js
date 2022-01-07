029ea0e1ad3e3c5203300adcb6cce02bbbed4fb9
David Preda <dtpreda30@gmail.com>
2022-01-07 20:42:08 +0000

    Add db.js dummy file

---
+++ server/db.js
@@ -0,0 +1,9 @@
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database(':memory:', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+    db.close();
+});
