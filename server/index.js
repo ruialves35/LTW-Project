fac08c50eee8f229c7721dddc4c7ad9c37863511
David Preda <dtpreda30@gmail.com>
2022-01-07 20:40:42 +0000

    Add app serving to server

--- server/index.js
+++ server/index.js
@@ -1,20 +1,46 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        let body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        }).on('end', () => {
-            body = Buffer.concat(body).toString();
-            console.log(body);
-        });
+    if (req.method == "POST") {
+        if (req.url === "/join") {
 
+        } else if (req.url === "/leave") {
 
-        let json = JSON.stringify({
-            data: 'Success',
-        });
-        res.end(json);
+        } else if (req.rul === "notify") {
+
+        } else if (req.url === "/ranking") {
+
+        } else if (req.url === "/register") {
+            
+        }
+    } else if (req.method == "GET") {
+        if (req.url === "/") {
+            res.writeHead(200, {
+                'Content-Type': 'text/html',
+            });
+            fs.readFile('./static/index.html', null, function (error, data) {
+                if (error) {
+                    res.writeHead(404);
+                    res.write('Whoops! File not found!');
+                } else {
+                    res.write(data);
+                }
+                res.end();
+            });
+        } else if (req.url === "/update") {
+
+        } else {
+            fs.readFile("./static/" + req.url, null, function (error, data) {
+                if (error) {
+                    res.writeHead(404);
+                    res.write('Whoops! File not found!');
+                } else {
+                    res.write(data);
+                }
+                res.end();
+            });
+        }
     }
 });
 
